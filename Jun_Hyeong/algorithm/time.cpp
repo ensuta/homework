857158aff0a1a723d2c43092ee5751a5fe8a8757
ensuta <ensuta08130813@gmail.com>
2020-08-09 23:03:41 +0900

    Update time.cpp

--- Jun_Hyeong/algorithm/time.cpp
+++ Jun_Hyeong/algorithm/time.cpp
@@ -5,11 +5,29 @@ using namespace std;
 
 int main ()
 {
-  int n, i;
+  int n, i, j, x;
 
   cin >> n;
-
-  for (i =0; i < n; i++) cout << i << endl;
+  //ONCE 'for' or 'while', N time repeat.
+  for (i =0; i < n; i++) {
+    for (j = 0; j < n; j++) x = i+j;
+  }
 
   return 0;
+
+}
+int bsearch(int a[], int n, int f)
+{
+   int left, right, mid;
+
+   left = 0;
+   right = n-1;
+   while (left <= right) {
+     mid = (left+right) / 2;
+     if (a[mid] == f) return mid;
+
+     if (a[mid] < f) left = mid+1;
+     else right = mid-1;
+   }
+   return -1;
 }
