388fb7aede92fb6132ee89b63c3b578c229a7d76
ensuta <ensuta08130813@gmail.com>
2020-09-15 10:37:17 +0900

    fix readme

--- autoBAND_attendancer/README.md
+++ autoBAND_attendancer/README.md
@@ -2,6 +2,8 @@
 
 <img src="http://www.woo-shin.hs.kr/files/2020/01/wooshin-h/a1c8457cc3564915a1cab551c7348795/01.gif" style="zoom:80%;" /> <img src="https://w.namu.la/s/fd8e16329f7ddbddb89dd1aedbea7c2e5f9f8dd54190559c6c40bdca55625378e3e3bb0ee76df741b4c4f20b8f9a12e4d15d2372598e2a358f978ea844c308fcb95ac7f206c38c5a00e2b5246eb89d7a1d03a6a9cdc87d26b96cde2612a5b15f" style="zoom:45%;" />
 
+#### <u font-color= #FFFF00>:::주의! 불안정합니다 Beta:::</u>
+
 울산 우신고등학교 1학년 학생을 위하여 만든 자동 출석체크 매크로 프로그램입니다.
 
 Firefox 위주로 개발되었습니다.
--- autoBAND_attendancer/main.py
+++ autoBAND_attendancer/main.py
@@ -3,15 +3,18 @@ import time
 
 logintime = input('로그인 시간 설정 (초) : ')
 bwr = webdriver.Firefox()
+bwr.get('https://auth.band.us/login?next_url=https%3A%2F%2Fband.us%2Fband%2F79345807')
+bwr.find_element_by_xpath('//*[@id="email_login_a"]').click()
+bwr.implicitly_wait(1)
 bwr.get('https://auth.band.us/email_login?keep_login=false')
 time.sleep(logintime)
 
-loop = 0
-while loop == 8:
-    loop = loop +1
+loop = 1 
+while loop > 0: 
     bwr.get('https://band.us/band/81335499/tag/태그1')
     bwr.implicitly_wait(5)
     bwr.find_element_by_xpath('/html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]').click()
     bwr.implicitly_wait(3)
     bwr.find_element_by_xpath('/html/body/div[1]/div[1]/main/div/section/div/div/div[4]/div[4]/div[2]/div/div[2]/ul/li/div/label').click()
-    time.sleep(60*60)
+    time.sleep(10)
+
