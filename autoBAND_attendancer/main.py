e6796119ac89f723b567b1b04a4100717b27e14e
ensuta <ensuta08130813@gmail.com>
2020-09-16 22:07:57 +0900

    fixed

--- autoBAND_attendancer/main.py
+++ autoBAND_attendancer/main.py
@@ -11,13 +11,13 @@ checkentr = '/html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/
 checkbtn = '/html/body/div[1]/div[2]/div/div/section/div[2]/div/section/div/div[4]/div[3]/div[2]/div/div[2]/ul/li/div/label'
 # /html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5] 실제 밴드 주소
 
-'''
+
 def loadpage(self):
     bwr.get(bandurl)
     time.sleep(jugi)
     fwrite
     listener
-'''
+
 
 print('2분뒤에 자동출첵이 시작, 로그인 수동')
 bwr.get('https://auth.band.us/login?next_url=https%3A%2F%2Fband.us%2Fband%2F79345807')
@@ -29,10 +29,7 @@ checkentrtxt = checkfind.text
 fwrite = open('temp.txt', 'w')
 fwrite.write(checkentrtxt)
 fwrite.close()
-bwr.get(bandurl)
-time.sleep(jugi)
-fwrite
-listener
+loadpage
 
 def listener(self):
     if checkentrtxt != fwrite.readline: 
@@ -42,15 +39,9 @@ def listener(self):
         checkfind()
         fwrite.write(checkentrtxt)
         fwrite.close()
-        bwr.get(bandurl)
-        time.sleep(jugi)
-        fwrite
-        listener
+        loadpage
     else:
-        bwr.get(bandurl)
-        time.sleep(jugi)
-        fwrite
-        listener
+        loadpage
 
 def checking(self):
         # 출석체크 항목으로 이동 (해결됨)
@@ -59,7 +50,4 @@ def checking(self):
         # 출석체크 체크 버튼 누름
     bwr.find_element_by_xpath(checkbtn).click()
     time.sleep(1)
-    bwr.get(bandurl)
-    time.sleep(jugi)
-    fwrite
-    listener
+    loadpage
