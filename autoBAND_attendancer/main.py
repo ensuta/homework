2e808080d6e7ca7ab43acdbb9a4c8b6b19f17e60
ensuta <ensuta08130813@gmail.com>
2020-09-17 18:21:38 +0900

    replace def to while

--- autoBAND_attendancer/main.py
+++ autoBAND_attendancer/main.py
@@ -2,52 +2,58 @@ from selenium import webdriver
 import time
 import os
 
-jugi = 60
-logintime = 110
 bwr = webdriver.Firefox()
+
+logintime = 110
+jugi = 60
+delay = 2
 bandurl = 'https://band.us/band/81335499/hashtag/%ED%83%9C%EA%B7%B81'
-checkentr = '/html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]/div/div/p[2]'
-# /html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]/div/div/p[2] 실제 밴드 주소
+checkentr = '/html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]'
+# /html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5] 출첵 항목
+checkboxtxt = '/html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]/div/div/p[2]'
+# /html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5]/div/div/p[2] 출첵 제목
 checkbtn = '/html/body/div[1]/div[2]/div/div/section/div[2]/div/section/div/div[4]/div[3]/div[2]/div/div[2]/ul/li/div/label'
-# /html/body/div[1]/div[1]/main/section/div[4]/div/div[1]/div/div[2]/div[2]/div/div[5] 실제 밴드 주소
 
-
-def loadpage(self):
-    bwr.get(bandurl)
-    time.sleep(jugi)
-    fwrite
-    listener
-
-
+# 수동으로 로그인
 print('2분뒤에 자동출첵이 시작, 로그인 수동')
 bwr.get('https://auth.band.us/login?next_url=https%3A%2F%2Fband.us%2Fband%2F79345807')
 time.sleep(logintime)
+
+#사이트로 이동 (최초)
 bwr.get(bandurl)
-time.sleep(10)
-checkfind = bwr.find_element_by_xpath(checkentr)
-checkentrtxt = checkfind.text
-fwrite = open('temp.txt', 'w')
-fwrite.write(checkentrtxt)
-fwrite.close()
-loadpage
+time.sleep(delay)
 
-def listener(self):
-    if checkentrtxt != fwrite.readline: 
-        checking()
-        bwr.refresh()
-        time.sleep(10)
-        checkfind()
-        fwrite.write(checkentrtxt)
-        fwrite.close()
-        loadpage
-    else:
-        loadpage
+i = 1
+while i > 0:
+    # 출첵항목의 제목 불러오고 임시저장
+    xpath = bwr.find_element_by_xpath(checkboxtxt)
+    chcktxt = xpath.text
+    print(chcktxt)
+    f = open('temp.txt', 'w')
 
-def checking(self):
+    if chcktxt != f.readline:
+
         # 출석체크 항목으로 이동 (해결됨)
-    bwr.find_element_by_xpath(checkentr).click()
-    time.sleep(3)
+        bwr.find_element_by_xpath(checkentr).click()
+        time.sleep(delay)
         # 출석체크 체크 버튼 누름
-    bwr.find_element_by_xpath(checkbtn).click()
-    time.sleep(1)
-    loadpage
+        bwr.find_element_by_xpath(checkbtn).click()
+        time.sleep(delay)
+        # 다시 불러오기
+        bwr.get(bandurl)
+        time.sleep(delay)
+        # 기록된 값 바꾸기
+        bwr.find_element_by_xpath(checkboxtxt)
+        chcktxt=chcktxt
+        f.write(chcktxt)
+        f.close()
+        # 새로고침
+        bwr.get(bandurl)
+        time.sleep(jugi)
+        bwr.get(bandurl)
+        time.sleep(delay)
+    else:
+        # 새로고침
+        time.sleep(jugi)
+        bwr.get(bandurl)
+        time.sleep(delay)
