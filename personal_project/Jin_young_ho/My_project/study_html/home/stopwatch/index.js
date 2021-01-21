5298cb190febcbb05e95d0866afd82b121501889
bate1206 <63945350+bate1206@users.noreply.github.com>
2021-01-21 16:52:36 +0900

    Add files via upload

---
+++ personal_project/Jin_young_ho/My_project/study_html/home/stopwatch/index.js
@@ -0,0 +1,78 @@
+
+var time = 0;
+var starFlag = true;
+
+$(document).ready(function(){
+  buttonEvt();
+});
+
+function init(){
+  document.getElementById("time").innerHTML = "00:00:00";
+}
+
+function buttonEvt(){
+  var hour = 0;
+  var min = 0;
+  var sec = 0;
+  var timer;
+
+  // start btn
+  $("#startbtn").click(function(){
+
+    if(starFlag){
+      $(".fa").css("color","#FAED7D")
+      this.style.color = "#4C4C4C";
+      starFlag = false;
+
+      if(time == 0){
+        init();
+      }
+
+      timer = setInterval(function(){
+        time++;
+
+        min = Math.floor(time/60);
+        hour = Math.floor(min/60);
+        sec = time%60;
+        min = min%60;
+
+        var th = hour;
+        var tm = min;
+        var ts = sec;
+        if(th<10){
+        th = "0" + hour;
+        }
+        if(tm < 10){
+        tm = "0" + min;
+        }
+        if(ts < 10){
+        ts = "0" + sec;
+        }
+
+        document.getElementById("time").innerHTML = th + ":" + tm + ":" + ts;
+      }, 1000);
+    }
+  });
+
+  // pause btn
+  $("#pausebtn").click(function(){
+    if(time != 0){
+      $(".fa").css("color","#FAED7D")
+      this.style.color = "#4C4C4C";
+      clearInterval(timer);
+      starFlag = true;
+    }
+  });
+
+  // stop btn
+  $("#stopbtn").click(function(){
+    if(time != 0){
+      $(".fa").css("color","#FAED7D")
+      this.style.color = "#4C4C4C";
+      clearInterval(timer);
+      starFlag = true;
+      time = 0;
+      init();
+    }
+  });
+}
